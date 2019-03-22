b0dd2ed48754f893c641bf0bd9949d9549fe75f1
Khalid Williams <khalidwill27@gmail.com>
2019-03-22 10:44:43 -0600

    Make testing (ping) and usergroup listing routes

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,64 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 
+import fetch from 'node-fetch';
 
 import express from 'express';
 const app = express();
 import bodyParser from 'body-parser';
-const PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
+app.get('/ping', (req, res) => {
+    res.send("pong")
+})
+
+app.get('/list-user-groups', (req, res) =>{
+    const baseUrl = "https://slack.com/api/usergroups.list";
+    // const reqUrl = baseUrl+'?token='+process.env.S_SECRET;
+    const reqUrl = baseUrl+'?token='+process.env.U_TOKEN;
+
+    
+
+    console.log(reqUrl)
+    const requestData = {
+        token: process.env.SECRET,
+    }
+
+    fetch(reqUrl, {
+        method: 'GET',
+        // body: new URLSearchParams(requestData),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlenconded'
+        }
+    })
+    .then((response) => {
+        if (response.ok) {
+            // console.log(response.keys())
+            // res.status(response.status).json(response.body)
+            console.log('In first then')
+            // console.log(response)
+            return response.json()
+        } else {
+            console.log(`I'm not ok (in first then)`)
+            let error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+        }
+    })
+    .then(json => {
+        console.log(`in second then`)
+        console.log(json)
+        res.send(json);
+    })
+    .catch(error => {
+        console.log('Been caught')
+        res.status = error.statusCode;
+        console.log(error)
+    })
+})
 
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(8080, () => console.log(`App running on port 8080`));
